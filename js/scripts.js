6bb9e10fc7909575742b370e27d8a80f242b2cc3
fuaad001 <husseinfuaad@hotmail.com>
2019-02-21 15:09:10 +0300

    Add second screen navigation query

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,21 @@ $(document).ready(function(){
     $(".card").show();
   });
 });
+
+//Open second screen
+$(document).ready(function(){
+  $(".card").submit(function(event){
+    $(".secondscreen").show();
+    $(".firstscreen").hide();
+
+    var nameInput =$("input#name").val();
+    var classInput = $("input#class").val();
+    var admissionInput = $("input#admission").val();
+
+    $(".name1").text(nameInput);
+    $(".class1").text(classInput);
+    $(".admission1").text(admissionInput);
+
+    event.preventDefault();
+  });
+});
